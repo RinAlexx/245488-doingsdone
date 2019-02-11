4b88640ca00db6e1f685c682fd89e41916e1cf25
RinAlexx <43240047+RinAlexx@users.noreply.github.com>
2019-02-11 23:50:14 +0300

    Поправила отступ, удалила

--- index.php
+++ index.php
@@ -45,9 +45,9 @@ $tasks = [
 
 function item_count($task_array, $name_of_project) {
 	$item_sum = 0;
-	foreach ($task_array as $key => $value) {
+	foreach ($task_array as $value) {
 		if ($value['category'] == $name_of_project) {
-		$item_sum += 1;
+			$item_sum += 1;
 		}
 	}
 	return $item_sum;
