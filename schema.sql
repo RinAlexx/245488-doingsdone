e72a456b0c9953fad6da959058b6e7a3a6b57d41
RinAlexx <43240047+RinAlexx@users.noreply.github.com>
2019-02-20 22:40:13 +0300

    Добавила файл со схемой БД

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE doingsdone
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+USE doingsdone;
+
+CREATE table projects (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	project_name CHAR(128) NOT NULL
+);
+
+CREATE table tasks (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	project_id INT NOT NULL,
+	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	complete_date TIMESTAMP,
+	status TINYINT(1) DEFAULT NULL,
+	task_name CHAR(128) NOT NULL,
+	file CHAR,
+	deadline TIMESTAMP NOT NULL
+);
+
+CREATE table users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	email CHAR(128) NOT NULL UNIQUE,
+	login CHAR(30) NOT NULL UNIQUE,
+	password CHAR(30) NOT NULL
+);
+
+CREATE UNIQUE INDEX mail ON users(email);
+CREATE UNIQUE INDEX user ON users(login);
+
+CREATE INDEX status ON tasks(status);
+CREATE INDEX project_id ON tasks(project_id);
+CREATE INDEX deadline ON tasks(deadline);
